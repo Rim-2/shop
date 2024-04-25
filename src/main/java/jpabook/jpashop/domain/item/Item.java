4593ca209495ec618fdf4b6e9c330522a9ae3a2c
Rim-2 <turtle2260@gmail.com>
2024-04-25 23:14:38 +0900

    feat: Add entity

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속관계 매핑
+@DiscriminatorColumn(name = "dtype") // 하위 타입 구분
+@Getter
+@Setter
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+}
